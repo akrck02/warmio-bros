60f58c5d773e0467a7ee240fa4eeee95fea1d69e
akrck02 <aketza.vazquez@gmail.com>
2024-09-05 00:59:15 +0200

    unified spritesheet, parallax fix and levels

--- entites/secret_tile/secret_tile.gd
+++ entites/secret_tile/secret_tile.gd
@@ -4,7 +4,12 @@ extends StaticBody2D
 @onready var jump_area : Area2D = $JumpArea
 @export var enabled : bool = true
 
-@export var secret_node : String = 'res://entites/enemies/nodes/gloonda/gloonda.tscn'
+enum SecretType {
+	Gloonda,
+	Coin
+}
+
+@export var secret_type : SecretType = SecretType.Gloonda
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,10 +24,17 @@ func show_secret(actor : Node2D) -> void:
 	enabled = false
 	animation.play("show")
 	
-	var scene = load(secret_node)
+	var scene = load(_get_current_secret_scene_path())
 	var node = scene.instantiate()
 	node.position = Vector2(position.x, position.y - 84)
 	get_parent().call_deferred(&'add_child', node)
 	
 	print("secret for %s" % actor.name)
 	pass
+
+func _get_current_secret_scene_path() -> String:
+	match secret_type:
+		SecretType.Gloonda:
+			return 'res://entites/enemies/nodes/gloonda/gloonda.tscn'
+
+	return 'res://entites/coin/coin.tscn'
--- managers/display_manager.gd
+++ managers/display_manager.gd
@@ -2,3 +2,10 @@ extends Node
 
 func _ready() -> void:
 	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed(&'exit'):
+		get_tree().quit()
+		
+	if Input.is_action_just_pressed(&'reset'):
+		get_tree().reload_current_scene()
